ab1af0afeca25317a67e60f853dbf5c00950c3b1
danillobr <danillorodriguesabreu@gmail.com>
2023-02-09 15:33:12 -0300

    Propriedades

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -3,10 +3,14 @@ import {Post} from "./Post"
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author= "Danillo Rodrigues" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, laudantium esse nesciunt nemo fugiat asperiores ullam earum atque magnam explicabo, odit deleniti repudiandae quidem expedita eveniet, a eum. Exercitationem, optio."
+      />
+      <Post
+        author= "Gabriel Buzzi" 
+        content="Um novo post show de bola"
+      />
     </div>
   )
 }
