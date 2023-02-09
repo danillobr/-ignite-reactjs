bf62ecbb29c26d10bd451d1a4f5fae5b575e06ea
danillobr <danillorodriguesabreu@gmail.com>
2023-02-09 18:06:21 -0300

    CSS Modules

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,11 +1,17 @@
-import {Post} from "./Post"
+import { Header } from "./components/Header";
+import {Post} from "./Post";
+
+import './styles.css'
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post
         author= "Danillo Rodrigues" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, laudantium esse nesciunt nemo fugiat asperiores ullam earum atque magnam explicabo, odit deleniti repudiandae quidem expedita eveniet, a eum. Exercitationem, optio."
+        con
+        tent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, laudantium esse nesciunt nemo fugiat asperiores ullam earum atque magnam explicabo, odit deleniti repudiandae quidem expedita eveniet, a eum. Exercitationem, optio."
       />
       <Post
         author= "Gabriel Buzzi" 
