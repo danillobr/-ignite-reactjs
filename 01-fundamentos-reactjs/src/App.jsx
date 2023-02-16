b8aed214af5c64e58f52ac1fd21c27c03474833b
danillobr <danillorodriguesabreu@gmail.com>
2023-02-16 16:01:43 -0300

    Iterando no JSX

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -6,6 +6,67 @@ import styles from './App.module.css';
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://i.pinimg.com/originals/c7/91/4f/c7914fe9d034a01769502de04c4a0e9b.jpg',
+      name: 'Roronoa Zoro',
+      role: 'Pirata'
+    },
+    content: [
+      {
+        type: 'paragraph',
+        context: 'Fala galeraa 👋'
+      },
+
+      {
+        type: 'paragraph',
+        context: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+      },
+
+      {
+        type: 'link',
+        context: '👉 jane.design/doctorcare'
+      }
+        
+      // <a href="#"> #novoprojeto </a>
+      // <a href="#"> #nlw </a>
+      // <a href="#"> #rocketseat </a>             
+    ],
+    publishedAt: new Data('2022-13-02 23:57:39')
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://lh3.googleusercontent.com/Q3E9kTWwLlA4m-49ph6Vmz157mfnGUHIgUg1hicUuZQu7mUzclYag0G55DVAPKvGxXCcknerA4_Xv468rfZLQVgnKuMIy_p-sp16wwU',
+      name: 'Usopp',
+      role: 'Pirata'
+    },
+    content: [
+      {
+        type: 'paragraph',
+        context: 'Fala galeraa 👋'
+      },
+
+      {
+        type: 'paragraph',
+        context: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+      },
+
+      {
+        type: 'link',
+        context: '👉 jane.design/doctorcare'
+      }
+        
+      // <a href="#"> #novoprojeto </a>
+      // <a href="#"> #nlw </a>
+      // <a href="#"> #rocketseat </a>             
+    ],
+    publishedAt: new Data('2022-16-02 11:54:22')
+  }
+]
 
 export function App() {
   return (
@@ -15,8 +76,7 @@ export function App() {
       <div className={styles.wrapper}> 
         <Sidebar />
         <main>   
-          <Post />
-          <Post />
+          {posts}
         </main>
       </div>     
     </div>
